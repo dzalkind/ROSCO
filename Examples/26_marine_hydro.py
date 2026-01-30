2a51a615fa4c6a0cbd7d065a7bc19cb702933aba
dzalkind <dzalkind@nrel.gov>
2026-01-30 15:49:05 -0700

    Make shorter MHK example

--- Examples/26_marine_hydro.py
+++ Examples/26_marine_hydro.py
@@ -8,6 +8,7 @@ import os
 from rosco.toolbox.ofTools.case_gen.run_FAST import run_FAST_ROSCO
 from rosco.toolbox.ofTools.case_gen import CaseLibrary as cl
 
+TEST_RUN = True
 
 def main():
     #directories
@@ -18,25 +19,26 @@ def main():
 
     # Input yaml and output directory
     parameter_filename = os.path.join(this_dir,'Tune_Cases/RM1_MHK.yaml')
-    run_dir = os.path.join(example_out_dir,'26_MHK/0_baseline')
+    run_dir = os.path.join(example_out_dir,'26_MHK_retest/0_baseline')
     os.makedirs(run_dir,exist_ok=True)
 
 
     # simulation set up
     r = run_FAST_ROSCO()
     r.tuning_yaml   = parameter_filename
-    # r.wind_case_fcn = cl.simp_step  # single step wind input
     r.wind_case_fcn = cl.power_curve
-    r.wind_case_opts    = {
-        'U': [2.5],
-        'TMax': 100.0,
-        }
+    if TEST_RUN:
+        r.wind_case_opts   = {
+            'TMax': 2.0,
+            }
+    else:
+        r.wind_case_opts   = {
+            'TMax': 100.0,
+            }
+    r.wind_case_opts['U'] = [2.5]
     r.case_inputs = {}
-    # r.fst_vt        = reader.fst_vt
-    # r.controller_params = controller_params
     r.save_dir      = run_dir
     r.rosco_dir     = rosco_dir
-    # r.rosco_dll     = '/Users/dzalkind/Tools/ROSCO-PRC/rosco/controller/build/libdiscon.dylib'
 
     r.run_FAST()
 

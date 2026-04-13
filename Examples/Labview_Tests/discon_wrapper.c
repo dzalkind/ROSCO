973ec9b544ad29e260c6ec8ad7475d0a0b213062
dzalkind <dzalkind@nrel.gov>
2026-04-13 16:02:27 -0600

    Add shutdown library

--- Examples/Labview_Tests/discon_wrapper.c
+++ Examples/Labview_Tests/discon_wrapper.c
@@ -23,6 +23,7 @@
   #include <direct.h>
   #define EXPORT __declspec(dllexport)
   #define OPEN_LIB(name)   LoadLibraryA(name)
+    #define CLOSE_LIB(h)     FreeLibrary(h)
   #define GET_SYM(h, sym)  GetProcAddress(h, sym)
   typedef HMODULE lib_handle_t;
   #define LIB_NAME "C:\\Users\\schamot\\Documents\\Code\\ROSCO-C\\rosco\\controller\\build\\win32-mingw\\liblibdiscon_win32.dll"
@@ -31,6 +32,7 @@
   #include <dlfcn.h>
   #define EXPORT
   #define OPEN_LIB(name)   dlopen(name, RTLD_NOW)
+    #define CLOSE_LIB(h)     dlclose(h)
   #define GET_SYM(h, sym)  dlsym(h, sym)
   typedef void* lib_handle_t;
   #define LIB_NAME "/Users/dzalkind/Tools/ROSCO-C/rosco/lib/libdiscon.dylib"
@@ -47,36 +49,59 @@ typedef void (*DISCON_fn)(float*, int*, const char*, const char*, char*);
 static lib_handle_t discon_lib = NULL;
 static DISCON_fn    discon_fn  = NULL;
 
+EXPORT void shutdown_discon(void) {
+    if (discon_lib) {
+        CLOSE_LIB(discon_lib);
+        discon_lib = NULL;
+        discon_fn = NULL;
+    }
+}
+
 static int load_discon(char* avcMSG) {
     if (discon_fn) return 0;  /* already loaded */
 
     const char* logName = "C:\\Users\\schamot\\Documents\\Code\\ROSCO-USFLOWT\\Examples\\Labview_Tests\\Logs\\discon_log.txt";
     const char* message = "Loading DISCON library";
     FILE* log_file = fopen(logName, "a");
-    fprintf(log_file, "%s\n", message);
+    if (log_file) {
+        fprintf(log_file, "%s\n", message);
+    }
     
     discon_lib = OPEN_LIB(LIB_NAME);
     if (!discon_lib) {
         #ifdef _WIN32
-        fprintf(log_file, "Failed to load %s: error %lu\n", LIB_NAME, GetLastError());
+        if (log_file) {
+            fprintf(log_file, "Failed to load %s: error %lu\n", LIB_NAME, GetLastError());
+            fclose(log_file);
+        }
         snprintf(avcMSG, MSG_SIZE, "Failed to load %s: error %lu", LIB_NAME, GetLastError());
         #else
         snprintf(avcMSG, MSG_SIZE, "Failed to load %s: %s", LIB_NAME, dlerror());
-        fprintf(log_file, "Failed to load %s: %s\n", LIB_NAME, dlerror());
+        if (log_file) {
+            fprintf(log_file, "Failed to load %s: %s\n", LIB_NAME, dlerror());
+            fclose(log_file);
+        }
         #endif
         return -1;
     }
-    fprintf(log_file, "Successfully loaded %s\n", LIB_NAME);
+    if (log_file) {
+        fprintf(log_file, "Successfully loaded %s\n", LIB_NAME);
+    }
     
     discon_fn = (DISCON_fn) GET_SYM(discon_lib, "DISCON");
     if (!discon_fn) {
-        fprintf(log_file, "Failed to find DISCON symbol in %s\n", LIB_NAME);
+        if (log_file) {
+            fprintf(log_file, "Failed to find DISCON symbol in %s\n", LIB_NAME);
+            fclose(log_file);
+        }
+        shutdown_discon();
         snprintf(avcMSG, MSG_SIZE, "Failed to find DISCON symbol in %s", LIB_NAME);
-        fclose(log_file);
         return -1;
     }
     
-    fclose(log_file);
+    if (log_file) {
+        fclose(log_file);
+    }
     return 0;
 }
 
@@ -116,8 +141,10 @@ EXPORT void run_discon(
     FILE* log_file;
 
     const char* logName = "C:\\Users\\schamot\\Documents\\Code\\ROSCO-USFLOWT\\Examples\\Labview_Tests\\Logs\\discon_log.txt";
-    log_file = fopen(logName, "w");
-    fprintf(log_file, "%s\n", message);
+    log_file = fopen(logName, "a");
+    if (log_file) {
+        fprintf(log_file, "%s\n", message);
+    }
     
     if (load_discon(avcMSG) != 0) {
         *aviFAIL = -1;
@@ -145,12 +172,17 @@ EXPORT void run_discon(
     avrSWAP[48] = (float)MSG_SIZE;
     avrSWAP[49] = (float)strlen(DISCON_IN);
     avrSWAP[50] = (float)strlen(SIM_NAME);
-    fprintf(log_file, "DISCON_IN length: %d, SIM_NAME length: %d\n", (int)avrSWAP[49], (int)avrSWAP[50]);
-    fclose(log_file);
+    if (log_file) {
+        fprintf(log_file, "DISCON_IN length: %d, SIM_NAME length: %d\n", (int)avrSWAP[49], (int)avrSWAP[50]);
+    }
     char msg_buf[MSG_SIZE] = {0};
     discon_fn(avrSWAP, aviFAIL, DISCON_IN, SIM_NAME, msg_buf);
-    fprintf(log_file, "DISCON returned aviFAIL=%d, msg=%s\n", *aviFAIL, msg_buf);
+    if (log_file) {
+        fprintf(log_file, "DISCON returned aviFAIL=%d, msg=%s\n", *aviFAIL, msg_buf);
+        fclose(log_file);
+    }
     strncpy(avcMSG, msg_buf, MSG_SIZE - 1);
+    avcMSG[MSG_SIZE - 1] = '\0';
     
     /* Extract outputs */
     *gen_torque = avrSWAP[46];

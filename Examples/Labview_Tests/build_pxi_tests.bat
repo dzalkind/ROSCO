596f41f1dfd6b949b0de7f029788fcd47aea3b38
dzalkind <dzalkind@nrel.gov>
2026-05-13 15:03:36 -0600

    Allow multiple definitions

--- Examples/Labview_Tests/build_pxi_tests.bat
+++ Examples/Labview_Tests/build_pxi_tests.bat
@@ -10,28 +10,30 @@ echo Compiling VEH stubs...
 gcc -m32 -c -o veh_stubs.o veh_stubs.c
 if errorlevel 1 (echo FAILED to compile veh_stubs.c & goto :eof)
 
+set LFLAGS=-m32 -shared -static -static-libgcc -static-libstdc++ -Wl,--kill-at,--allow-multiple-definition
+
 echo Building test_1 (minimal C++) ...
-g++ -m32 -shared -static -static-libgcc -static-libstdc++ -DBUILD_TEST1 -o test_1.dll pxi_tests.c veh_stubs.o -Wl,--kill-at
+g++ %LFLAGS% -DBUILD_TEST1 -o test_1.dll pxi_tests.c veh_stubs.o
 if errorlevel 1 (echo FAILED & goto :eof)
 
 echo Building test_2 (new/delete) ...
-g++ -m32 -shared -static -static-libgcc -static-libstdc++ -DBUILD_TEST2 -o test_2.dll pxi_tests.c veh_stubs.o -Wl,--kill-at
+g++ %LFLAGS% -DBUILD_TEST2 -o test_2.dll pxi_tests.c veh_stubs.o
 if errorlevel 1 (echo FAILED & goto :eof)
 
 echo Building test_3 (std::string) ...
-g++ -m32 -shared -static -static-libgcc -static-libstdc++ -DBUILD_TEST3 -o test_3.dll pxi_tests.c veh_stubs.o -Wl,--kill-at
+g++ %LFLAGS% -DBUILD_TEST3 -o test_3.dll pxi_tests.c veh_stubs.o
 if errorlevel 1 (echo FAILED & goto :eof)
 
 echo Building test_4 (C file I/O) ...
-g++ -m32 -shared -static -static-libgcc -static-libstdc++ -DBUILD_TEST4 -o test_4.dll pxi_tests.c veh_stubs.o -Wl,--kill-at
+g++ %LFLAGS% -DBUILD_TEST4 -o test_4.dll pxi_tests.c veh_stubs.o
 if errorlevel 1 (echo FAILED & goto :eof)
 
 echo Building test_5 (C++ streams) ...
-g++ -m32 -shared -static -static-libgcc -static-libstdc++ -DBUILD_TEST5 -o test_5.dll pxi_tests.c veh_stubs.o -Wl,--kill-at
+g++ %LFLAGS% -DBUILD_TEST5 -o test_5.dll pxi_tests.c veh_stubs.o
 if errorlevel 1 (echo FAILED & goto :eof)
 
 echo Building test_6 (strings + malloc + snprintf + getenv) ...
-g++ -m32 -shared -static -static-libgcc -static-libstdc++ -DBUILD_TEST6 -o test_6.dll pxi_tests.c veh_stubs.o -Wl,--kill-at
+g++ %LFLAGS% -DBUILD_TEST6 -o test_6.dll pxi_tests.c veh_stubs.o
 if errorlevel 1 (echo FAILED & goto :eof)
 
 echo.
